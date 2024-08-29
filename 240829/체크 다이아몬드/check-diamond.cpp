5549d516441779c06d291111589d582fb3ffef79
김식혜 <139305586+GimSikhye@users.noreply.github.com>
2024-08-29 12:29:35 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240829/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+
+	//윗부분
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n-i-1; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	//아랫부분
+	for (int i = n-2; i >= 0; i--)
+	{ 
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "* ";
+		}
+		cout << endl;
+	}
+
+
+}
